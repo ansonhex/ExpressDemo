24759311b1055b0ef82642a5b16b935bed784c2a
ansonhe97 <ansonhe1997@gmail.com>
2024-05-09 18:16:40 -0700

    small fix

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
+import methodOverride from "method-override";
 
 const port = 3000;
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+// added DEL to HTML using method-override
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
