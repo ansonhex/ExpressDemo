02f766afd0ed12030b81bbea799055df7fd91f45
ansonhe97 <ansonhe1997@gmail.com>
2024-05-09 23:13:14 -0700

    added main frame home page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import methodOverride from "method-override";
 import mongoose from "mongoose";
+import articleRouter from "./routes/articles.js"
 
 const port = 3000;
 const app = express();
@@ -12,9 +13,23 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 // added DEL to HTML using method-override
 app.use(methodOverride("_method"));
+app.set("view engine", "ejs");
+
+// Mount router
+app.use("/articles", articleRouter);
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    const articles = [{
+        title: "TEST title",
+        createdAt: new Date(),
+        description: "TEST DECRI"
+    },
+    {
+        title: "TEST title2",
+        createdAt: new Date(),
+        description: "TEST DECRI"
+    }]
+    res.render("index", {articles});
 });
 
 app.listen(port, () => {
