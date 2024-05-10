bc85ef7021d99e71b7dfec1cfa270ced5acce9dd
ansonhe97 <ansonhe1997@gmail.com>
2024-05-09 18:45:51 -0700

    add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import methodOverride from "method-override";
+import mongoose from "mongoose";
 
 const port = 3000;
 const app = express();
